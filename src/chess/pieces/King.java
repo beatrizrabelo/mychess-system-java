891c0cc878748347416be3eb31697aa824f40aae
Beatriz Rabelo <beatriz.devrabelo@gmail.com>
2022-01-06 00:20:20 -0300

    Little Improvement in board printing

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -14,6 +14,7 @@ public class King extends ChessPiece {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
+    @Override
     public String toString(){
         return "K";
     }
